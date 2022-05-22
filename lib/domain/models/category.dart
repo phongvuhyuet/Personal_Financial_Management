d4ed3859e74b84f140bc28dab45e67490e248636
phongltktv <vuphonghyuet@gmail.com>
2022-05-22 18:40:03 +0700

    api: create category + get categories's name

--- lib/domain/models/category.dart
+++ lib/domain/models/category.dart
@@ -4,33 +4,26 @@ import 'package:equatable/equatable.dart';
 class Category extends Equatable {
   String id;
   String name;
+  String user_id;
   bool? isOutput;
 
   Category({
     required this.id,
     required this.name,
+    required this.user_id,
     this.isOutput,
   });
-  factory Category.fromSnapshot(DocumentSnapshot snapshot) {
-    final transaction =
-        Category.fromJson(snapshot.data() as Map<String, dynamic>);
-    transaction.id = snapshot.reference.id;
-    return transaction;
-  }
   factory Category.fromJson(Map<String, dynamic> json) => Category(
-        id: json["id"],
-        name: json["name"],
-        isOutput: json["is_output"],
-      );
+      id: json["id"],
+      name: json["name"],
+      isOutput: json["is_output"],
+      user_id: json["user_id"]);
   Map<String, dynamic> toJson() => {
         "id": id,
         "name": name,
         "isOutput": isOutput,
+        "user_id": user_id,
       };
   @override
-  List<Object?> get props => [
-        id,
-        name,
-        isOutput,
-      ];
+  List<Object?> get props => [id, name, isOutput, user_id];
 }
